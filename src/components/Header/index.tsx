de0af06a464d27b74329ff3736e125ab84b5790c
Uallesson Nunes Ivo <uallessons@gmail.com>
2021-10-21 05:25:11 -0300

    primeira etapa finalizada, aplicação ainda em dese

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,15 @@
 import React from "react";
+// import LogoSvg from "../../assets/logo.svg";
 import { Text, View } from "react-native";
-import { styles } from "./styles";
-import { Header } from "../../components/Header";
 
-export function Home() {
+import { styles } from "./styles";
+
+export function Header() {
   return (
     <View style={styles.container}>
-      <Header />
+      {/* <LogoSvg /> */}
+
+      <Text style={styles.logoutText}>Sair</Text>
     </View>
   );
 }
-
-export default Home;
